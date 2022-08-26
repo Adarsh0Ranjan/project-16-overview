d45fdb1586f96c1eef44352be54aefa5c859528a
adarsh <adarsh0ranjan@gmail.com>
2022-08-26 08:13:10 +0530

    Initial Commit

---
+++ project-16-overview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-16-overview
+//
+//  Created by Roro Solutions on 26/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project-16-overview/project_16_overviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_16_overviewApp.swift
+//  project-16-overview
+//
+//  Created by Roro Solutions on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct project_16_overviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
